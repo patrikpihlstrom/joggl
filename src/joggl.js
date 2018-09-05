6e99e39c6af59aea8a0b0ce2b902a5034e796234
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2018-09-05 22:42:25 +0200

    log issue-id

--- src/joggl.js
+++ src/joggl.js
@@ -1,16 +1,26 @@
 jQuery(document).ready(function ($) {
+    function openPopup(e, id) {
+        console.log(id);
+	};
+
 	// Insert the button elements
 	function prependButtons() {
 		$(".ListItem__item:not(.joggled)").each(function () {
 			if ($(this).find('.joggl').length == 0) {
-				$(this).addClass('joggled');
-				$(this).prepend('<div class="ContextMenu__container joggl"><img src="' + browser.extension.getURL("assets/images/joggl.png") + '" class="joggl-button"/></div>');
+			    var id = null;
+			    try {
+                    id = $(this).find('.css-ly1zji0')[0].innerText.split(' ')[0];
+                    $(this).addClass('joggled');
+                    $(this).prepend('<div class="ContextMenu__container joggl" id="' + id + '"><img src="' +
+						browser.extension.getURL("assets/images/joggl.png") + '" class="joggl-button"/></div>');
+                    document.getElementById(id).addEventListener('click', function (e) {
+                    	openPopup($(e.srcElement.parentElement.parentElement)[0], id);
+                    });
+				} catch (e) {
+
+				}
 			}
 		});
-	}
+	};
     setInterval(prependButtons, 1000);
-
-	$('.joggl-button').click(function () {
-		console.log('clicked');
-	});
 });
