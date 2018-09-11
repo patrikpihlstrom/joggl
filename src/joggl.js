be62ca698e960d18e1cddb44e7677d3dfc00ffc2
Patrik Pihlström <patrik.pihlstrom@caupo.se>
2018-09-11 07:46:16 +0200

    fixed a bug where the first time entry created two work logs.

--- src/joggl.js
+++ src/joggl.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function ($) {
-    var url = null, token = null;
+	$(document).keypress(function (event) {
+		console.log(event.which);
+    });
+
+    var url = null, token = null, available = true;
     function getSettings() {
         function setCurrentChoice(result) {
             url = result.url;
@@ -15,7 +19,14 @@ jQuery(document).ready(function ($) {
     }
     getSettings();
     function createWorklog(id, data) {
+        if (!available)
+        {
+            return false;
+        }
+
+        available = false;
         data = JSON.stringify(data);
+        console.log('creating worklog: ' + data);
         $.ajax({
             url: url + 'rest/api/2/issue/' + id + '/worklog',
             type: 'POST',
@@ -27,6 +38,8 @@ jQuery(document).ready(function ($) {
                 xhr.setRequestHeader('User-Agent', 'Jira won\'t accept FireFox\'s user-agent string for some reason...');
             },
             complete: function(response) {
+				available = true;
+            	console.log(response);
                 if (response.statusText === 'Created')
                 {
                     $('#' + id + '-popup').hide();
@@ -41,41 +54,43 @@ jQuery(document).ready(function ($) {
 	function prependButtons() {
 		$(".ListItem__item:not(.joggled)").each(function () {
 			if ($(this).find('.joggl').length == 0) {
-			    var id = null;
-			    try {
+                var id = null;
+                try {
                     id = $(this).find('.css-ly1zji0')[0].innerText.split(' ')[0];
-                    $(this).addClass('joggled');
-                    $(this).prepend('<div class="ContextMenu__container joggl" id="' + id + '"><img src="' +
-						browser.extension.getURL("assets/images/joggl.png") + '" class="joggl-button"/></div><div id="'
-                        + id + '-popup' + '" class="popup" style="z-index: 1000">' +
-                        '<span class="close-popup" id="' + id + '-close">X</span>' +
-                        '<input type="text" id="timeSpent" placeholder="Time Spent, e.g. 1h 30m"/>' +
-                        '<input type="text" id="comment" placeholder="Comment"/>' +
-                        '<button id="create-log-' + id + '">Create Worklog</button></div>');
-                    document.getElementById(id).addEventListener('click', function (e) {
-                        $('.popup').each(function () {
-                            $(this).hide();
+                    if (id.length > 0) {
+                        $(this).addClass('joggled');
+                        $(this).prepend('<div class="ContextMenu__container joggl" id="' + id + '"><img src="' +
+                            browser.extension.getURL("assets/images/joggl.png") + '" class="joggl-button"/></div><div id="'
+                            + id + '-popup' + '" class="popup" style="z-index: 1000">' +
+                            '<span class="close-popup" id="' + id + '-close">X</span>' +
+                            '<input type="text" id="timeSpent" placeholder="Time Spent, e.g. 1h 30m"/>' +
+                            '<input type="text" id="comment" placeholder="Comment"/>' +
+                            '<button id="create-log-' + id + '">Create Worklog</button></div>');
+                        document.getElementById(id).addEventListener('click', function (e) {
+                            $('.popup').each(function () {
+                                $(this).hide();
+                            });
+                            var popup = $('#' + e.target.parentElement.id + '-popup');
+                            popup.show();
                         });
-                        var popup = $('#' + e.srcElement.parentElement.id + '-popup');
-                        popup.show();
-                    });
-                    document.getElementById('create-log-' + id).addEventListener('click', function (e) {
-                        var popup = $('#' + e.srcElement.parentElement.id);
-                        var data = {
-                            timeSpent: popup.find('#timeSpent')[0].value,
-                            started: new Date().toISOString().split('.')[0] + '.000-0000',
-                            comment: popup.find('#comment')[0].value
-                        };
-                        createWorklog(id, data);
-                    });
-                    document.getElementById(id + '-close').addEventListener('click', function (e) {
-                        $(e.srcElement.parentElement).hide();
-                    });
+                        document.getElementById('create-log-' + id).addEventListener('click', function (e) {
+                            var popup = $('#' + e.target.parentElement.id);
+                            var data = {
+                                timeSpent: popup.find('#timeSpent')[0].value,
+                                started: new Date().toISOString().split('.')[0] + '.000-0000',
+                                comment: popup.find('#comment')[0].value
+                            };
+                            createWorklog(id, data);
+                        });
+                        document.getElementById(id + '-close').addEventListener('click', function (e) {
+                            $(e.target.parentElement).hide();
+                        });
+                    }
 				} catch (e) {
-			        console.log(e);
-				}
-			}
-		});
-	};
+                    console.log(e);
+                }
+            }
+        });
+    };
     setInterval(prependButtons, 1000);
 });
